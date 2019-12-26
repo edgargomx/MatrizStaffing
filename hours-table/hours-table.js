4e599e1a80560ac094a9fc1ae792b3d541dc35fc
armandolezama <victor.lezama@ironbit.com.mx>
2019-12-26 00:06:23 -0600

    Chore(Were created hours-table component and demo, and files of WCT)

--- hours-table/hours-table.js
+++ hours-table/hours-table.js
@@ -1,10 +1,16 @@
 import { html, LitElement } from 'lit-element';
 import style from './hours-table-styles.js';
+import '@vaadin/vaadin-grid/vaadin-grid.js';
+import '@vaadin/vaadin-grid/vaadin-grid-column';
+import '@vaadin/vaadin-grid/vaadin-grid-selection-column';
+import '@vaadin/vaadin-grid/vaadin-grid-sort-column';
+import '@vaadin/vaadin-button/vaadin-button.js';
 
 class HoursTable extends LitElement {
   static get properties() {
     return {
-      hello: { type: String }
+      years : {
+        type: Object}
     };
   }
 
@@ -14,13 +20,46 @@ class HoursTable extends LitElement {
 
   constructor() {
     super();
-    this.hello = 'Hello';
+    this.url = '/Assets/Acciones.png'
+    this.years = [];
   }
+
+  renderAll(){
+    (async () => {
+      await customElements.whenDefined('vaadin-grid').then(() => {
+      const grid = this.shadowRoot.querySelector('vaadin-grid');
+  
+      const columns = this.shadowRoot.querySelectorAll('vaadin-grid-column');
+
+      columns[0].renderer = (root, column, rowData) => {
+        if (!root.firstElementChild) {
+          root.insertAdjacentHTML('afterbegin', `<img alt="action">`)
+        }
+        root.firstElementChild.id = rowData.item.year;
+        root.firstElementChild.src = this.url;
+      };
+
+      grid.items = this.years
+    })})().then(()=> {
+      const buttons = this.shadowRoot.querySelectorAll('img')
+      for(const button of buttons){
+        button.addEventListener('click', event => {
+          this.dispatchEvent(new CustomEvent('year-selected',
+          {detail: {year: event.currentTarget.id}})) 
+        })
+      }
+    })
+  }
+  updated() {
+    this.renderAll();
+}
 
   render() {
     return html`
-        <p>Some static DOM</p>
-        <h2>${this.hello} hours-table</h2>
+        <vaadin-grid theme="row-dividers" column-reordering-allowed multi-sort>
+            <vaadin-grid-sort-column width="9em" path="year" header="Año"></vaadin-grid-sort-column>
+            <vaadin-grid-column width="9em" header="Acciones"></vaadin-grid-sort-column>
+        </vaadin-grid>
       `;
     }
 }
