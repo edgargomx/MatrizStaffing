2c854c4389ed5f2c6db29c8f6e528bf0289ecadc
edgar.guzman <edgar.guzman@ironbit.com.mx>
2019-12-26 15:00:17 -0600

    feat(hour-form) add styles y refactor render

--- hours-form/hours-form-styles.js
+++ hours-form/hours-form-styles.js
@@ -3,12 +3,70 @@ import { css } from 'lit-element';
 export default css`
 :host {
   display: inline-block;
-  box-sizing: border-box; }
+  box-sizing: border-box;
+  --font-family: Verdana;
+  --label-color: #757575; }
 
 :host([hidden]), [hidden] {
   display: none !important; }
 
 *, *:before, *:after {
   box-sizing: inherit;
-  font-family: inherit; }
+  font-family: var(--font-family); }
+
+.col {
+  display: flex;
+  flex-direction: column; }
+
+.row {
+  display: flex;
+  flex-direction: row;
+  flex-flow: row wrap;
+  justify-content: flex-start; }
+
+.content-center {
+  justify-content: space-around;
+  align-items: center; }
+
+.content-evenly {
+  justify-content: space-evenly; }
+
+.content-left {
+  justify-content: space-between;
+  align-items: flex-start; }
+
+.center-item {
+  align-self: center; }
+
+.right-item {
+  align-self: flex-end; }
+
+.d-none {
+  display: none; }
+
+label {
+  margin-top: 12px;
+  font-family: var(--font-family);
+  width: 65px;
+  margin-right: 3px;
+  color: var(--label-color); }
+
+.margin-top-sm {
+  margin-top: 15px; }
+
+.margin-top-md {
+  margin-top: 45px; }
+
+.margin-top-lg {
+  margin-top: 60px; }
+
+.card {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  padding: 10px;
+  color: #757575;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2); }
 `;
--- hours-form/hours-form.js
+++ hours-form/hours-form.js
@@ -3,6 +3,7 @@ import style from './hours-form-styles.js';
 import '@vaadin/vaadin-text-field/vaadin-integer-field'
 import '@vaadin/vaadin-combo-box/vaadin-combo-box'
 import '@vaadin/vaadin-button/vaadin-button'
+import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles'; 
 
 class HoursForm extends LitElement {
   static get properties() {
@@ -52,25 +53,35 @@ class HoursForm extends LitElement {
   }
   
   send(){
-    const[janInput,febInput,marInput,aprInput,mayInput,
-      junInput,julInput,augInput,sepInput,octInput,
-      novInput,decInput] = this.shadowRoot.querySelectorAll('vaadin-integer-field');
-    const properties ={
-      year:this.yearSelected,
-      january:janInput.value,
-      february:febInput.value,
-      march:marInput.value,
-      april:aprInput.value,
-      may:mayInput.value,
-      june:junInput.value,
-      july:julInput.value,
-      august:augInput.value,
-      september:sepInput.value,
-      october:octInput.value,
-      november:novInput.value,
-      december:decInput.value
-    }
+    const [
+        janInput,
+        febInput,
+        marInput,
+        aprInput,
+        mayInput,
+        junInput,
+        julInput,
+        augInput,
+        sepInput,
+        octInput,
+        novInput,
+        decInput ] = this.shadowRoot.querySelectorAll('vaadin-integer-field');
 
+    const properties = {
+      year     :this.yearSelected,
+      january  :janInput.value,
+      february :febInput.value,
+      march    :marInput.value,
+      april    :aprInput.value,
+      may      :mayInput.value,
+      june     :junInput.value,
+      july     :julInput.value,
+      august   :augInput.value,
+      september :sepInput.value,
+      october  :octInput.value,
+      november :novInput.value,
+      december :decInput.value
+    }
 
     this.dispatchEvent(new CustomEvent('sendhrs',{
       detail: properties
@@ -79,69 +90,81 @@ class HoursForm extends LitElement {
 
   render() {
     return html`
-    <div class="col-center">
-      <h2>Agregar horas</h2>
-      <div class="col-left">
-        <div class="item">
-        <span for="year">Año</span>
-        <vaadin-combo-box @change="${this.changeYear}"></vaadin-combo-box>
+        <div class="col ">
+          <h2 class="center-item">Agregar horas</h2>
+          <div class="row content-center">
+            <div class="col">
+              <div class="row">
+                <label>Año: </label>
+                <vaadin-combo-box @change="${this.changeYear}" theme="custom-border"></vaadin-combo-box>
+              </div>
+              <div class="row margin-top-sm">
+                <label>Enero:</label>
+                <vaadin-integer-field id="january" min="0" ></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Febrero:</label>
+                <vaadin-integer-field id="february" min="0" ></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Marzo:</label>
+                <vaadin-integer-field id="march" min="0" ></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Abril:</label>
+                <vaadin-integer-field id="april" min="0" ></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Mayo:</label>
+                <vaadin-integer-field id="may" min="0" ></vaadin-integer-field>
+              </div>
+              <div class="row">
+                <label>Junio:</label>
+                <vaadin-integer-field id="june" min="0" ></vaadin-integer-field>
+              </div>
+            </div>
+            <div class="col">
+              <div class="row margin-top-lg">
+                <label>Julio:</label>
+                <vaadin-integer-field id="july" min="0"></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Agosto:</label>
+                <vaadin-integer-field id="august" min="0"></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Sep:</label>
+                <vaadin-integer-field id="september" min="0"></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Oct:</label>
+                <vaadin-integer-field id="october" min="0"></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Nov:</label>
+                <vaadin-integer-field id="november" min="0"></vaadin-integer-field>
+              </div>
+              <div class="row ">
+                <label>Dic:</label>
+                <vaadin-integer-field id="december" min="0"></vaadin-integer-field>
+              </div>
+            </div>
+          </div>
+          <div class="row content-center margin-top-md">
+            <vaadin-button @click="${this.send}" theme="primary">Guardar</vaadin-button>
+          </div>          
         </div>
-        <div class="item">
-          <span>Enero</span>
-          <vaadin-integer-field id="january" min="0" ></vaadin-integer-field>
-        </div>
-        <div class="item">
-          <span>Febrero</span>
-          <vaadin-integer-field id="february" min="0" ></vaadin-integer-field>
-        </div>
-        <div class="item">
-          <span>Marzo</span>
-          <vaadin-integer-field id="march" min="0" ></vaadin-integer-field>
-        </div>
-        <div class="item">
-          <span>Abril</span>
-          <vaadin-integer-field id="april" min="0" ></vaadin-integer-field>
-        </div>
-        <div class="item">
-          <span>Mayo</span>
-          <vaadin-integer-field id="may" min="0" ></vaadin-integer-field>
-        </div>
-        <div class="item">
-          <span>Junio</span>
-          <vaadin-integer-field id="june" min="0" ></vaadin-integer-field>
-        </div>
-      </div>
-      <div class="col-right">
-        <div class="item">
-          <span>Julio</span>
-          <vaadin-integer-field id="july" min="0"></vaadin-integer-field>
-        </div>
-        <div class="item">
-          <span>Agoto</span>
-          <vaadin-integer-field id="august" min="0"></vaadin-integer-field>
-        </div>
-        <div class="item">
-        <span>Sep.</span>
-          <vaadin-integer-field id="september" min="0"></vaadin-integer-field>
-        </div>
-        <div class="item">
-        <span>Oct.</span>
-          <vaadin-integer-field id="october" min="0"></vaadin-integer-field>
-        </div>
-        <div class="item">
-        <span>Nov.</span>
-          <vaadin-integer-field id="november" min="0"></vaadin-integer-field>
-        </div>
-        <div class="item">
-        <span>Dic.</span>
-          <vaadin-integer-field id="december" min="0"></vaadin-integer-field>
-        </div>
-      </div>
-      <vaadin-button @click="${this.send}">Guardar</vaadin-button>
-      
-    </div>
       `;
     }
 }
+
+registerStyles('vaadin-text-field', css`
+  :host([theme~="custom-border"]) [part="input-field"] {
+        width: 130px;
+      }
+`);
+
+
+
 
 window.customElements.define("hours-form", HoursForm);
