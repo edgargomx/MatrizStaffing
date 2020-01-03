531b08d137c0dbf7e0548daf50070c9e12c65bb4
armandolezama <victor.lezama@ironbit.com.mx>
2020-01-03 08:49:46 -0600

    Refactor(Vaadin components supplied by paper components)

--- hours-table/hours-table.js
+++ hours-table/hours-table.js
@@ -1,12 +1,9 @@
 import { html, LitElement } from 'lit-element';
 import style from './hours-table-styles.js';
-import '@vaadin/vaadin-grid/vaadin-grid.js';
-import '@vaadin/vaadin-grid/vaadin-grid-column';
-import '@vaadin/vaadin-grid/vaadin-grid-selection-column';
-import '@vaadin/vaadin-grid/vaadin-grid-sort-column';
-import '@vaadin/vaadin-button/vaadin-button.js';
-import '@ironbit/hours-form/hours-form.js';
-import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles';
+import '@fluidnext-polymer/paper-grid/paper-grid.js';
+import '@polymer/paper-button/paper-button.js';
+// import '@ironbit/hours-form/hours-form.js';
+// import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles';
 
 class HoursTable extends LitElement {
   static get properties() {
@@ -41,37 +38,47 @@ class HoursTable extends LitElement {
     }
   }
 
+  throwCells(){
+    let elements = [];
+    if(this.years.length > 0 ){
+      let i = 0;
+      for(const data in this.rawData){
+        elements = [...elements, this.__buildYearCell(i, data), this.__buildIcon(data, i)]
+        i++
+      }
+      return elements
+    } else {
+      return '<p>Not data jet</p>'
+    }
+
+  }
   __getHours(year){
-    this.hours = this.rawData[year]
-  }
-  __renderAll(){
-      customElements.whenDefined('vaadin-grid').then( () => {
-      const grid = this.shadowRoot.querySelector('vaadin-grid');
-  
-      const columns = this.shadowRoot.querySelectorAll('vaadin-grid-column');
-
-      columns[1].renderer = (root, column, rowData) => {
-        const icon = this.__buildIcon(rowData.item.year);
-        if (!root.firstElementChild) {
-          root.appendChild(icon);
-        }
-      };
-
-      grid.items = this.years;
-    }).then( () => {
-      const vaadinButton = this.shadowRoot.querySelector('vaadin-button')
-      vaadinButton.addEventListener('click', () => {
-        this.shadowRoot.querySelector('hours-form').display();
-        this.shadowRoot.querySelector('hours-form').create();
-      })
-    })
+    this.hours = this.rawData[year];
   }
 
-  __buildIcon(id){
+  __buildYearCell(row, text){
+    const yearCell = document.createElement('p');
+    const colAtt = document.createAttribute('col');
+    const rowAtt = document.createAttribute('row');
+    colAtt.value = 0;
+    rowAtt.value = row;
+    yearCell.setAttributeNode(colAtt);
+    yearCell.setAttributeNode(rowAtt)
+    yearCell.appendChild(document.createTextNode(text));
+    return yearCell;
+  }
+
+  __buildIcon(data, row){
     const icon = document.createElement('img');
+    const colAtt = document.createAttribute('col');
+    const rowAtt = document.createAttribute('row');
+    colAtt.value = 1;
+    rowAtt.value = row;
     icon.src = this.url;
     icon.alt = this.url;
-    icon.id = id
+    icon.id = data;
+    icon.setAttributeNode(colAtt);
+    icon.setAttributeNode(rowAtt);
     icon.addEventListener('click', event => {
       const form = this.shadowRoot.querySelector('hours-form');
       form.display();
@@ -83,28 +90,43 @@ class HoursTable extends LitElement {
     return icon
   }
 
+  __renderAll(){
+      const grid = this.shadowRoot.querySelector('#grid');
+      customElements.whenDefined(grid.localName).then(() => {
+        for(const cell of this.throwCells()){
+          grid.appendChild(cell);
+        }
+      })
+  }
+
+  __addEventListenerToAllIcons(){
+    const icons = document.querySelector('#icon');
+    for(const icon of icons){
+      icon.addEventListener('click', event => {
+        const form = this.shadowRoot.querySelector('hours-form');
+        form.display();
+        form.edit();
+        this.__getHours(event.currentTarget.id);
+        form.set(event.currentTarget.id, this.hours);
+      })
+    }
+  }
+
   addNewYear(newYear){
     this.years = [...this.years, {year: newYear}];
     this.__renderAll();
   }
 
-  updated() {
-    this.__renderAll();
-  }
+  // updated() {
+  //   this.__renderAll();
+  // }
 
   firstUpdated(){
-    const form = this.shadowRoot.querySelector('hours-form');
-    customElements.whenDefined(form.localName).then(() => {
-        form.addEventListener('years-changed', event => {
-          this.__getHours(event.detail.year);
-          form.set(event.detail.year,  this.hours);
-        })
-        form.addEventListener('edited', event => {
-          this.dispatchEvent(new CustomEvent('edited', {detail: event.detail}))
-        })
-        form.addEventListener('created', event => {
-          this.dispatchEvent(new CustomEvent('created', {detail: event.detail}))
-        })
+    this.__renderAll();
+    const registerButton = this.shadowRoot.querySelector('paper-button');
+    registerButton.addEventListener('click', () => {
+      this.shadowRoot.querySelector('hours-form').display();
+      this.shadowRoot.querySelector('hours-form').create();
     })
   }
 
@@ -115,35 +137,15 @@ class HoursTable extends LitElement {
   render() {
     return html`
         <div class="col content-center">
-          <vaadin-grid theme="row-stripes" column-reordering-allowed multi-sort>
-              <vaadin-grid-column width="9em" path="year" header="Año"></vaadin-grid-column>
-              <vaadin-grid-column width="9em" header="Acciones"></vaadin-grid-column>
-          </vaadin-grid>
-          <vaadin-button theme="primary" class="margin-top-md">Registro</vaadin-button>
-          <hours-form></hours-form>
+          <paper-grid id="grid">
+            
+          </paper-grid>
+          <paper-button >Registrar</paper-button>
         </div>
+        <hours-form></hours-form>
       `;
     }
 }
 
-registerStyles('vaadin-grid', css`
-  :host([theme~="row-stripes"])
-      [part="row"]:last-child [part~="header-cell"] {
-        background: var(--background-gradient);
-        color: white;
-      }
-      [part~="cell"] {
-        text-align: center;
-      }
-`);
-
-registerStyles('vaadin-button', css`
-  :host([theme~="primary"]) {
-        background: var(--background-gradient);
-        border-radius: 30px;
-        font-size: var(--font-size-button);
-        width: 350px;
-      }
-`);
 
 window.customElements.define("hours-table", HoursTable);
